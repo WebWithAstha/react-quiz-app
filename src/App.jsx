f22dff4ea814f5b9e158cfee4c542f3414adddd6
WebWithAstha <lodhiastha85@gmail.com>
2024-06-24 23:31:08 +0530

    boiler plate code

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainRoutes from './routes/MainRoutes'
+import Intro from './components/Quizes'
+
+
+const App = () => {
+  return (
+    <>
+    <MainRoutes/>
+
+    </>
+  )
+}
+
+export default App
