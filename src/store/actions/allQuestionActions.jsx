6b3af59300086c54f5f197d52e9bf25dd051c225
WebWithAstha <lodhiastha85@gmail.com>
2024-06-26 22:54:05 +0530

    actions made

--- src/store/actions/allQuestionActions.jsx
+++ src/store/actions/allQuestionActions.jsx
@@ -2,10 +2,22 @@ import { load } from "../reducers/userQuizDataSlice"
 
 
 
-export const asyncSetQuestions = (ques)=>(dispatch, getState)=>{
-    console.log("I'm asynceSetQuestions")
-    dispatch(load(ques))
-    localStorage.setItem("userQuizData",JSON.stringify(ques))
+
+export const asyncLoad = (userQuizData)=>(dispatch, getState)=>{
+    dispatch(load(userQuizData))
+    localStorage.setItem("userQuizData",JSON.stringify(userQuizData))
+}
+export const asyncSetPage = (pageNo)=>(dispatch, getState)=>{
+    const userQuizData = {...getState().userQuizDataSlice.userQuizData}
+    userQuizData.pageNo = pageNo
+    dispatch(load(userQuizData))
+    localStorage.setItem("userQuizData",JSON.stringify(userQuizData))
 }
 
-
+export const asyncChangeAnswers = (answers) => (dispatch, getState) => {
+    const quizData = getState().userQuizDataSlice.userQuizData;
+    const copyQuizData = JSON.parse(JSON.stringify(quizData));
+    copyQuizData.answers = answers;
+    dispatch(load(copyQuizData));
+    localStorage.setItem("userQuizData", JSON.stringify(copyQuizData));
+};
